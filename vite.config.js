b0afd3d06bab18288cc85003f17d066e8ae3a1b6
Mandy Tsai <mandy3152618@gmail.com>
2021-11-24 11:24:01 +0800

    update vite add base

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,30 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
-import WindiCSS from "vite-plugin-windicss";
-import Components from "unplugin-vue-components/vite";
-import viteSvgIcons from "vite-plugin-svg-icons";
-import path from "path";
-import Icons from "unplugin-icons/vite";
-import IconsResolver from "unplugin-icons/resolver";
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import WindiCSS from 'vite-plugin-windicss'
+import Components from 'unplugin-vue-components/vite'
+import viteSvgIcons from 'vite-plugin-svg-icons'
+import path from 'path'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/christmas-2021/',
   plugins: [
     vue(),
     WindiCSS(),
     Components({
       resolvers: IconsResolver({
-        compiler: "vue3",
-        prefix: "Icon",
+        compiler: 'vue3',
+        prefix: 'Icon',
       }),
     }),
     Icons(),
     viteSvgIcons({
       // Specify the icon folder to be cached
-      iconDirs: [path.resolve(process.cwd(), "src/assets/icons")],
+      iconDirs: [path.resolve(process.cwd(), 'src/assets/icons')],
       // Specify symbolId format
-      symbolId: "icon-[dir]-[name]",
+      symbolId: 'icon-[dir]-[name]',
     }),
   ],
-});
+})
