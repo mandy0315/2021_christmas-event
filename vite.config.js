f7572e3a8cdb7acd9075914427f15ec8d00f0579
mandy <mandy3152618@gmail.com>
2026-02-03 11:31:01 +0800

    fix: base 設定相對路徑

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ import IconsResolver from 'unplugin-icons/resolver'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/christmas-2021/',
+  base: './',
   plugins: [
     vue(),
     WindiCSS(),
