9906f3abd8b449891251dbf0afc51a082d79c288
mandy <mandy3152618@gmail.com>
2026-02-03 11:19:43 +0800

    fix: 刪除 sass 相關

--- src/main.js
+++ src/main.js
@@ -1,8 +1,7 @@
-import { createApp } from "vue";
-import App from "./App.vue";
-import "./scss/all.scss";
-import "virtual:windi.css";
-import "virtual:windi-devtools";
-import "virtual:svg-icons-register";
+import { createApp } from 'vue'
+import App from './App.vue'
+import 'virtual:windi.css'
+import 'virtual:windi-devtools'
+import 'virtual:svg-icons-register'
 
-createApp(App).mount("#app");
+createApp(App).mount('#app')
